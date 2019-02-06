d31f63df840411e60d6365d932ad2c2e263f0a35
Nicholas Gill <mythagel@gmail.com>
2019-02-06 17:13:49 +1100

    Move geom::volume to ops_volume.cpp

--- src/ops.cpp
+++ src/ops.cpp
@@ -32,16 +32,6 @@
 
 #include "cgal.h"
 
-// Meshing (for volume)
-#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
-#include <CGAL/Mesh_triangulation_3.h>
-#include <CGAL/Mesh_complex_3_in_triangulation_3.h>
-#include <CGAL/Mesh_criteria_3.h>
-#include <CGAL/Polyhedral_mesh_domain_3.h>
-#include <CGAL/Polyhedral_mesh_domain_with_features_3.h>
-#include <CGAL/make_mesh_3.h>
-#include <CGAL/refine_mesh_3.h>
-
 // glide
 #include <CGAL/minkowski_sum_3.h>
 
@@ -50,15 +40,6 @@
 
 // subdivide
 #include <CGAL/Subdivision_method_3.h>
-
-// Meshing
-typedef CGAL::Exact_predicates_inexact_constructions_kernel Inexact_Kernel;
-typedef CGAL::Mesh_polyhedron_3<Inexact_Kernel>::type Mesh_polyhedron_3;
-
-typedef CGAL::Polyhedral_mesh_domain_with_features_3<Inexact_Kernel, Mesh_polyhedron_3> Mesh_domain;
-typedef CGAL::Mesh_triangulation_3<Mesh_domain>::type Tr;
-typedef CGAL::Mesh_complex_3_in_triangulation_3<Tr> C3t3;
-typedef CGAL::Mesh_criteria_3<Tr> Mesh_criteria;
 
 namespace geom
 {
@@ -84,46 +65,6 @@ polyhedron_t glide(const polyhedron_t& polyhedron, const polyline_t& path)
 		return make_polyhedron( std::move(priv) );
 
 	throw std::runtime_error("glide result is not 2-manifold");
-}
-
-double volume(const polyhedron_t& polyhedron)
-{
-	Mesh_polyhedron_3 PK;
-	{
-		typedef CGAL::Mesh_polyhedron_3<Nef_Kernel>::type Nef_Mesh_Polyhedron_3;
-	
-		Nef_Mesh_Polyhedron_3 EP;
-		get_priv(polyhedron)->nef.convert_to_polyhedron(EP);
-		copy_to(EP, PK);
-	}
-	assert(PK.is_valid());
-
-	Mesh_domain domain(PK);
-	domain.detect_features();
-
-	using CGAL::parameters::facet_angle;
-	using CGAL::parameters::facet_size;
-	using CGAL::parameters::facet_distance;
-	using CGAL::parameters::cell_radius_edge_ratio;
-	// facet_size = 0.15, facet_distance = 0.008
-	// TODO these parameters need to be tweaked.
-	Mesh_criteria criteria(facet_angle = 25, facet_size = 1, facet_distance = 0.8, cell_radius_edge_ratio = 3);
-
-	using CGAL::parameters::no_perturb;
-	using CGAL::parameters::no_exude;
-	auto c3t3 = CGAL::make_mesh_3<C3t3>(domain, criteria, no_perturb(), no_exude());
-
-	auto triangulation = c3t3.triangulation();
-
-	std::vector<double> volumes;
-	volumes.reserve(triangulation.number_of_finite_cells());
-	for(auto it = triangulation.finite_cells_begin(); it != triangulation.finite_cells_end(); ++it)
-	{
-		auto tetr = triangulation.tetrahedron(it);
-		volumes.push_back(tetr.volume());
-	}
-	std::sort(volumes.begin(), volumes.end());
-	return std::accumulate(volumes.begin(), volumes.end(), 0.0);
 }
 
 polyhedron_t merge(const std::vector<polyhedron_t>& polyhedra)
